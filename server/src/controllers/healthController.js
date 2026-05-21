5f2b167b13c8ab410ade257a02740d44a8bfb229
ShafinNigamana <shafin.nigamana@gmail.com>
2026-05-21 20:00:38 +0530

    feat: add dual database backend infrastructure

--- server/src/controllers/healthController.js
+++ server/src/controllers/healthController.js
@@ -1,6 +1,11 @@
-export const getHealthStatus = (req, res) => {
-  res.status(200).json({
-    status: 'OK',
-    message: 'API is healthy',
-  });
+import { getMongoHealthStatus } from '../config/mongo.js';
+import { getMySQLHealthStatus } from '../config/mysql.js';
+
+export const getHealthStatus = async (req, res) => {
+  const [mongo, mysql] = await Promise.all([getMongoHealthStatus(), getMySQLHealthStatus()]);
+
+  const payload = { mongo, mysql };
+  const hasFailure = Object.values(payload).some((status) => status !== 'ok');
+
+  res.status(hasFailure ? 503 : 200).json(payload);
 };
